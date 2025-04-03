80982a642d1577d214b6166c10f8efd0c9d04b1a
James Fan <jmfan2002@gmail.com>
2025-04-03 00:10:28 -0400

    nicer devices list

--- scripts/devices_list.gd
+++ scripts/devices_list.gd
@@ -1,17 +1,26 @@
 extends ItemList
 
 var selected_devices: PackedInt32Array
+var devices_lookup: Array
+
+var python_cmd := "import sounddevice as sd; import json;
+device_dict = {}
+for index, dev in enumerate(sd.query_devices()):
+	if dev['max_input_channels'] > 0 and dev['name'] not in device_dict:
+		device_dict[dev['name']] = index
+print(json.dumps(device_dict))"
 
 func _ready() -> void:
 	clear()
 
-	# TODO: process the output to look nicer + exclude output devices
 	var output: Array[String] = []
-	OS.execute("python", ["-m", "sounddevice"], output)
-	var devices: PackedStringArray = output.pop_back().split("\n")
-	for device in devices:
-		add_item(device)
-
+	OS.execute("python", ["-c", python_cmd], output)
+	var devices: Dictionary = JSON.parse_string(output.pop_back())
+	print(devices)
+	for name in devices:
+		add_item(name)
+		devices_lookup.push_back(devices[name])
 
 func _process(delta: float) -> void:
-	selected_devices = get_selected_items()
+	var items := get_selected_items()
+	selected_devices = Array(items).map(func(idx): return devices_lookup[idx])
