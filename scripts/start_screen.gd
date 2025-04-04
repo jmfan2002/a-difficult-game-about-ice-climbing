cb3a5baaa7a518f3d530ba6648370ee0ee135817
James Fan <jmfan2002@gmail.com>
2025-04-04 03:00:22 -0400

    how to

--- scripts/start_screen.gd
+++ scripts/start_screen.gd
@@ -1,10 +1,14 @@
 extends Control
 
-var next_scene = preload("res://scenes/Level.tscn")
+var level_scene = preload("res://scenes/Level.tscn")
+var how_to_scene =preload("res://scenes/How_to.tscn")
 
 func _process(_delta: float) -> void:
 	$Play_button.disabled = $Devices_list.selected_devices.size() != 2
 
 func _on_play_button_pressed() -> void:
 	Global.selected_devices = Array($Devices_list.selected_devices)
-	get_tree().change_scene_to_packed(next_scene)
+	get_tree().change_scene_to_packed(level_scene)
+
+func _on_how_to_button_pressed() -> void:
+	get_tree().change_scene_to_packed(how_to_scene)
